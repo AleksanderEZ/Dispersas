6d5869e2f368adee7637146b0a1de2cc8e230758
Aleksander <57395146+AleksanderEZ@users.noreply.github.com>
2021-11-18 17:29:47 +0000

    no00

--- main.cpp
+++ main.cpp
@@ -22,51 +22,69 @@ void printArray(double* data, int length) {
 	printf("%lf ]\n", data[length - 1]);
 }
 
+void printArray(float* data, int length) {
+	printf("[ ");
+	for (int i = 0; i < length - 1; i++)
+	{
+		printf("%f, ", data[i]);
+	}
+	printf("%f ]\n", data[length - 1]);
+}
+
 void actividadPractica4ejercicio1() {
 	//Resolver el siguiente sistema Ax=b aplicando la metodología DSS
 
-	//int A[25] = {
-	//	1, 0, 0, 0, 0,
-	//	2, 3, 0, 0, 0,
-	//	4, 0, 5, 0, 0,
-	//	0, 6, 0, 7, 0,
-	//	0, 0, 8, 0, 9
-	//};
-
-	int A[9] = {
-		1, 2, 3, 4, 5, 6, 7, 8, 9
+	float A[25] = {
+		1, 0, 0, 0, 0,
+		2, 3, 0, 0, 0,
+		4, 0, 5, 0, 0,
+		0, 6, 0, 7, 0,
+		0, 0, 8, 0, 9
 	};
+	float B[5] = { 1, 2, 3, 4, 5 };
 
-	int B[5] = { 1, 2, 3, 4, 5 };
+	float Avalues[9];
+	int rowIndex[6];
+	int colIndex[9];
+	const int m = 5;
 
-	int rowIndex[5 + 1] = { 0, 1, 3, 5, 7, 9 };
-	int colIndex[9] = { 0, 0, 1, 0, 2, 1, 3, 2, 4 };
+	// Paso a CSR
+
+	int job[6] = { 0,0,0,2,10,3 };
+	MKL_INT info;
+	mkl_sdnscsr(job, &m, &m, A, &m, Avalues, colIndex, rowIndex, &info);
+	printArray(Avalues, 9);
+	printArray(rowIndex, 6);
+	printArray(colIndex, 9);
+
+	// Resolución con DSS
 
 	const MKL_INT size = 5;
 	const MKL_INT nonZeroAmount = 9;
 
 	_MKL_DSS_HANDLE_t handle;
-	MKL_INT opt = MKL_DSS_ZERO_BASED_INDEXING + MKL_DSS_SINGLE_PRECISION + MKL_DSS_MSG_LVL_WARNING + MKL_DSS_TERM_LVL_ERROR;
-	int info = dss_create(handle, opt);
+	MKL_INT opt = MKL_DSS_ZERO_BASED_INDEXING + MKL_DSS_SINGLE_PRECISION 
+		+ MKL_DSS_MSG_LVL_WARNING + MKL_DSS_TERM_LVL_ERROR;
+	info = dss_create(handle, opt);
 	printf("Create: %d\n", info);
 
 	opt = MKL_DSS_NON_SYMMETRIC;
 	info = dss_define_structure(handle, opt, rowIndex, size, size, colIndex, nonZeroAmount);
 	printf("Structure: %d\n", info);
 
-	int* perm = (int*) malloc(5 * sizeof(int));
 	opt = MKL_DSS_AUTO_ORDER;
 	info = dss_reorder(handle, opt, 0);
 	printf("Reorder: %d\n", info);
 
 	opt = MKL_DSS_INDEFINITE;
-	info = dss_factor_real(handle, opt, A);
+	info = dss_factor_real(handle, opt, Avalues);
 	printf("Factor: %d\n", info);
 
 	printArray(B, 5);
-	opt = MKL_DSS_FORWARD_SOLVE;
-	info = dss_solve_real(handle, opt, B, size, B);
-	printArray(B, 5);
+	float solValues[25];
+	opt = MKL_DSS_DIAGONAL_SOLVE;
+	info = dss_solve_real(handle, opt, B, size, solValues);
+	printArray(solValues, 25);
 	printf("Solve real: %d\n", info);
 
 	opt = MKL_DSS_MSG_LVL_WARNING + MKL_DSS_TERM_LVL_ERROR;
@@ -86,6 +104,7 @@ void actividadPractica2ejercicio1() {
 	};
 	
 	// CSR
+	printf("Codificacion CSR\n");
 	int job[6] = { 0,0,0,2,40,3 };
 	int ia[8];
 	int ja[16];
@@ -95,33 +114,44 @@ void actividadPractica2ejercicio1() {
 	int nnz = 16;
 
 	mkl_ddnscsr(job, &m, &m, A, &m, acsr, ja, ia, &info);
+	printf("values:\n");
 	printArray(acsr, 16);
+	printf("colid:\n");
 	printArray(ja, 16);
+	printf("rowid:\n");
 	printArray(ia, 8);
 	printf("info: %d\n", info);
 
 	// COO
+	printf("Codificacion COO\n");
 	int job2[6] = { 0,0,0,2,40,3 };
 	int rowind[16];
 	int colind[16];
 	double acoo[16];
 
 	mkl_dcsrcoo(job2, &m, acsr, ja, ia, &nnz, acoo, rowind, colind, &info);
+	printf("values:\n");
 	printArray(acoo, 16);
+	printf("rowid:\n");
 	printArray(rowind, 16);
+	printf("colid:\n");
 	printArray(colind, 16);
 	printf("info: %d\n", info);
 
-	// BSR XD no se puede porque matrix 7x7
+	// BSR no se puede porque matrix 7x7
 
 	// CSC
+	printf("Codificacion CSC\n");
 	int job3[6] = { 0,0,0,0,0,3 };
 	double absr[16];
 	int jab[16];
 	int iab[8];
 	mkl_dcsrcsc(job3, &m, acsr, ja, ia, absr, jab, iab, &info);
+	printf("values:\n");
 	printArray(absr, 16);
+	printf("colid:\n");
 	printArray(jab, 16);
+	printf("rowid:\n");
 	printArray(iab, 8);
 	printf("info: %d\n", info);
 }
